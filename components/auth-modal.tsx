c4c25b5182d3106f8a40aff1f2a85de55db86d03
Zhuangda Zhu <zhuangdaz@gmail.com>
2025-11-09 18:42:31 -0500

    test

--- components/auth-modal.tsx
+++ components/auth-modal.tsx
@@ -106,7 +106,7 @@ export function AuthModal({ open, onOpenChange, onSuccess, trigger = 'manual', c
   const getURL = () => {
     let url =
       process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      'http://localhost:3000/'
+      'http://localhost:8000/'
     // Make sure to include `https://` when not localhost.
     url = url.startsWith('http') ? url : `https://${url}`
     // Make sure to include a trailing `/`.
