ea10c615baefcfa9560faad4490661d04bacea2d
Zhuangda Zhu <zhuangdaz@gmail.com>
2025-11-09 19:23:16 -0500

    Update auth-modal.tsx

--- components/auth-modal.tsx
+++ components/auth-modal.tsx
@@ -108,7 +108,7 @@ export function AuthModal({ open, onOpenChange, onSuccess, trigger = 'manual', c
     let url =
       process?.env?.NEXT_PUBLIC_APP_URL ?? // Set this to your site URL in production env.
       process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      'http://localhost:3000/'
+      'http://localhost:9000/'
     // Make sure to include `https://` when not localhost.
     url = url.startsWith('http') ? url : `https://${url}`
     // Make sure to include a trailing `/`.
